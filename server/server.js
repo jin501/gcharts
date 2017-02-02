0b1bf6d77bd4376dc7d0389f1b039ba5360d0061
jin501 <soljin01@gmail.com>
2017-02-01 20:43:42 -0500

    Set up environment

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+
+const hello = "meow";
